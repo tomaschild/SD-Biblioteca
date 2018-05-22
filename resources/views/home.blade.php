332a1253b1fd1080a818a10d460640e36f60b049
Tomás Child <tomas.child@usach.cl>
2018-05-22 12:35:25 -0400

    Primer commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Biblioteca Digital</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="{{asset('css/home.css')}}">
+	<link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet'>
+	<link href='https://fonts.googleapis.com/css?family=Allerta' rel='stylesheet'>
+	<link href='https://fonts.googleapis.com/css?family=Archivo Narrow' rel='stylesheet'>
+</head>
+<body>
+	<div class="container_fondo">
+		<div class="fondo">
+			<div class="titulo">Biblioteca Digital</div>
+
+			<div class="contenedor">
+				<div class="centrador"></div>
+				<div class="contenedor_barra_busqueda">
+					<input class="barra_busqueda" type="text" name="busqueda">
+				</div>
+				<div class="centrador"></div>
+			</div>
+		</div>
+
+	</div>
+</body>
+</html>
---
+++ resources/views/results.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Resultados de Búsqueda</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="{{asset('css/results.css')}}">
+	<link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet'>
+	<link href='https://fonts.googleapis.com/css?family=Allerta' rel='stylesheet'>
+	<link href='https://fonts.googleapis.com/css?family=Archivo Narrow' rel='stylesheet'>
+</head>
+<body>
+	<div class="container_banner">
+		<div class="banner"></div>
+	</div>
+
+	<div class="titulo_busqueda">Mostrando resultados para: "hola mundo"</div>
+	<div class="container_resultados">
+		<div class="item_resultado">Resultado 1</div>
+		<div class="item_resultado">Resultado 2</div>
+		<div class="item_resultado">Resultado 3</div>
+		<div class="item_resultado">Resultado 4</div>
+		<div class="item_resultado">Resultado 5</div>
+		<div class="item_resultado">Resultado 6</div>
+	</div>
+
+</body>
+</html>
